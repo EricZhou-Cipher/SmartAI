18661e57be523ff2916ca53a9fc0880035429827
EricZhou-Cipher <zhoueric115@gmail.com>
2025-03-11 23:05:55 +0800

    fix: 将 Jest 和 Babel 配置文件改为 .cjs 扩展名，解决 ES 模块兼容性问题

---
+++ backend/jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.cjs' }],
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testMatch: ['**/tests/**/*.test.js', '**/src/tests/**/*.test.js'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/tests/**', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testTimeout: 30000,
+  transformIgnorePatterns: ['node_modules/(?!(axios|uuid|winston)/)'],
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+};
