e2d37286525529de0d53a4a3ecc1aca1e206e3a6
EricZhou-Cipher <zhoueric115@gmail.com>
2025-03-12 18:06:34 +0800

    修复 CI/CD 配置：更新 Node.js 版本，添加工作目录设置，确保 Babel 和 Jest 配置正确

--- ChainIntelAI/backend/babel.config.js
+++ ChainIntelAI/backend/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+  ],
 };
--- ChainIntelAI/backend/jest.config.cjs
+++ ChainIntelAI/backend/jest.config.cjs
@@ -6,6 +6,12 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
-  transformIgnorePatterns: ['/node_modules/(?!(@opentelemetry)/)'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@opentelemetry|ethers|uuid|zod|mongoose|axios|redis|ioredis|telegraf|ts-retry-promise)/)',
+  ],
   setupFilesAfterEnv: ['./jest.setup.js'],
+  testTimeout: 30000,
+  verbose: true,
+  forceExit: true,
+  detectOpenHandles: true,
 };
