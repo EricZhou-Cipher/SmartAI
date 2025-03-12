1a6cd85510eacbbcf2a4696ab4ad47f18f7a850b
EricZhou-Cipher <zhoueric115@gmail.com>
2025-03-12 23:21:42 +0800

    在本地babel.config.js中强制添加@babel/plugin-transform-modules-commonjs插件

--- ChainIntelAI/backend/babel.config.js
+++ ChainIntelAI/backend/babel.config.js
@@ -8,7 +8,8 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-modules-commonjs'
   ],
   sourceType: 'unambiguous'
 };
